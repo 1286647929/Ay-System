de48bec6d643d4b89ae4abcba138e3a9ae49ef87
AyCode <1286647929@qq.com>
2023-05-04 22:00:51 +0800

    第一次提交

---
+++ Ay-backend/src/main/java/com/ay/AyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyBackendApplication.class, args);
+    }
+
+}
